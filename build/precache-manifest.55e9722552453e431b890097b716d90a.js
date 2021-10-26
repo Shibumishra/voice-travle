554d1a24392842881115f0ea3213d0e0659e5119
Shibumishra <sibumishra487@gmail.com>
2021-10-26 17:53:18 +0530

    build

---
+++ build/precache-manifest.55e9722552453e431b890097b716d90a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c720575a798ab133f8d94d859b7733ef",
+    "url": "/voice-travle/index.html"
+  },
+  {
+    "revision": "60d42bfc240269c0fe6f",
+    "url": "/voice-travle/static/css/main.0f677a17.chunk.css"
+  },
+  {
+    "revision": "88f98386014fe6da3673",
+    "url": "/voice-travle/static/js/2.b6475d3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voice-travle/static/js/2.b6475d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d42bfc240269c0fe6f",
+    "url": "/voice-travle/static/js/main.3815f3fd.chunk.js"
+  },
+  {
+    "revision": "485db1c41214193a8116",
+    "url": "/voice-travle/static/js/runtime-main.8351ef76.js"
+  }
+]);
